965cbe1d92cfdd629b87e495d38c17d10ef9eddc
ajskateboarder <81255936+themysticsavages@users.noreply.github.com>
2021-05-24 11:38:22 -0700

    0.1.6

---
+++ listevery/__init__.py
@@ -0,0 +1,9 @@
+"""
+listevery
+
+Get every single folder and file in directories and subdirectories!
+"""
+
+__version__ = "0.1.6"
+__author__ = 'themysticsavages'
+__credits__ = 'No one'
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+        name='listevery',
+        version='0.1.6',
+        url='https://github.com/themysticsavages/listevery',
+        author='themysticsavages',
+        description="A file lister on steroids",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        author_email='noemailprovidedthankyou@nonexistentemailservice.com',
+        license='Apache',
+        packages=['listevery'],
+        python_requires=">=3.6",
+
+        classifiers = [
+            'Programming Language :: Python :: 3',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: OS Independent',
+        ]
+)
